f829ffbfe180da4a1aee4510007dac138cbecf8f
iftekharrafti <iftekharrafti7040@gmail.com>
2025-02-22 13:55:35 +0600

    Add ThemeOneProductCategory component and integrate it into ThemeOne; adjust HeroSection styling

--- src/components/Theme/ThemeOne/ThemeOne.jsx
+++ src/components/Theme/ThemeOne/ThemeOne.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import MainTemplate from '../../templates/MainTemplate/MainTemplate';
 import ThemeOneMainTemplate from '../../templates/ThemeOneMainTemplate/ThemeOneMainTemplate';
 import ThemeOneHeroSection from '../../organisms/ThemeOneHeroSection/ThemeOneHeroSection';
+import ThemeOneProductCategory from '../../organisms/ThemeOneProductCategory/ThemeOneProductCategory';
 
 const ThemeOne = () => {
     return (
         <div>
             <ThemeOneMainTemplate>
                 <ThemeOneHeroSection />
+                <ThemeOneProductCategory />
             </ThemeOneMainTemplate>
         </div>
     );
